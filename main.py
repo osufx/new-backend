00d6144ac7b028491e5f27c965640d907d9c22c8
Sunpy <sunpy@osufx.com>
2018-01-09 09:08:50 +0100

    json response for error page

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
-from flask import Flask, make_response, redirect, request, render_template, url_for, flash
+import random
+from flask import Flask, make_response, redirect, request, render_template, url_for, flash, jsonify
 
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
@@ -9,9 +10,27 @@ def main_index():
 with open("config.json", "r") as f:
     config = json.load(f)
 
+with open("api_response.json", "r") as f:
+    api_response = json.load(f)
+
 @app.errorhandler(404)
 def not_found(error):
-    return '404'
+    lookup = "common"
+
+    if random.randint(0, 5) != 0 and str(error.code) in api_response.keys():
+        lookup = str(error.code)
+
+    msg = api_response[lookup]
+
+    #Randomize
+    msg = msg[random.randint(0, len(msg) - 1)]
+
+    res = {
+        "code": error.code,
+        "message": msg
+    }
+
+    return jsonify(**res)
 
 if __name__ == "__main__":
     app.run(**config)
