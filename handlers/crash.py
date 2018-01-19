a6a20ac3a52f8ceddf1f80ebb7268ed6940d8549
Sunpy <sunpy@osufx.com>
2018-01-19 11:29:57 +0100

    Incase same user pushes crashlog the same timestamp

--- handlers/crash.py
+++ handlers/crash.py
@@ -1,16 +1,23 @@
 import os.path
 import time
 import glob
+from pathlib import Path
 
 def handle_crash(request):
     filename = str(int(time.time()))
 
     if "u" in request.args:
         filename += "-{}".format(request.args.get("u"))
-        
-    full_filename = os.path.join(glob.config["handle"]["crash"], filename + ".txt")
+    
+    full_filename = None
+    
+    full_filename = os.path.join(glob.config["handle"]["crash"], filename + "-{}.txt")
 
-    file = open(full_filename, "w")
+    file_id = 0
+    while Path(full_filename.format(file_id)).is_file():
+        file_id += 1
+
+    file = open(full_filename.format(file_id), "w")
 
     for arg in request.args:
         file.write("{}: {}\n".format(arg, request.args.get(arg)))
